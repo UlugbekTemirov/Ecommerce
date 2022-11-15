0529f4cbdbeac6de0a61d559e9c6b0f1e379921d
UlugbekTemirov <temirovulugbek2003@gmail.com>
2022-11-15 11:59:39 +0500

    `auth modified`

--- src/index.js
+++ src/index.js
@@ -3,28 +3,17 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-<<<<<<< HEAD
-=======
 // react-toastify
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
->>>>>>> 42aa146 (`auth modified`)
 import App from "./App";
 import ThemeMode from "./UI/ThemeMode";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< HEAD
-  <React.StrictMode>
     <ThemeMode>
       <App />
+      <ToastContainer />
     </ThemeMode>
-  </React.StrictMode>
-=======
-  <ThemeMode>
-    <App />
-    <ToastContainer />
-  </ThemeMode>
->>>>>>> 42aa146 (`auth modified`)
 );

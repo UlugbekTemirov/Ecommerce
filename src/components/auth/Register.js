ceb58b65fa6110a67a78672d8178cc41516434cf
UlugbekTemirov <temirovulugbek2003@gmail.com>
2022-11-15 12:09:58 +0500

    `initial changes`

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -68,7 +68,7 @@ const Register = () => {
           onChange={getFullNameHandler}
           type="text"
           name="text"
-          className="mt-1 px-3 py-2 text-black bg-slate-200 bg-white border shadow-sm border-slate-300 placeholder-slate-500 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+          className="mt-1 px-3 py-2 text-black bg-slate-200 border shadow-sm border-slate-300 placeholder-slate-500 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
           placeholder="John Doe"
         />
       </label>
@@ -81,7 +81,7 @@ const Register = () => {
           onChange={getEmailHandler}
           type="email"
           name="email"
-          className="mt-1 px-3 py-2 text-black bg-slate-200 bg-white border shadow-sm border-slate-300 placeholder-slate-500 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+          className="mt-1 px-3 py-2 text-black bg-slate-200 border shadow-sm border-slate-300 placeholder-slate-500 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
           placeholder="you@example.com"
         />
       </label>
